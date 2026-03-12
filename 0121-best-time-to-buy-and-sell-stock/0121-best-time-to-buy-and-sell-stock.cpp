d67e8d6c4b981966194d34cea914636b262e9225
Rittik Chakraborty <rittikchakraborty24@gmail.com>
2026-03-12 16:44:07 +0530

    Time: 4 ms (17.76%), Space: 97.4 MB (23.97%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void mpFounder(vector<int>& prices, int& i, int& minPrice, int& maxProfit){
+        //Base Case
+        if(i>=prices.size()) return;
+
+        // One Case
+        if(prices[i] < minPrice) minPrice = prices[i];
+        if((prices[i] - minPrice) > maxProfit) maxProfit = (prices[i] - minPrice);
+
+        //RE
+        mpFounder(prices, ++i, minPrice, maxProfit);
+    }
+
+    int maxProfit(vector<int>& prices) {
+        int minPrice = INT_MAX, maxProfit = 0, i=0;
+        mpFounder(prices, i, minPrice, maxProfit);
+        return maxProfit;
+    }
+};
