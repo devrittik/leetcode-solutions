bdf739a855c59d76c016d80f3d8a048cbad1d527
Rittik Chakraborty <rittikchakraborty24@gmail.com>
2026-03-12 16:49:12 +0530

    Time: 0 ms (100%), Space: 97.4 MB (23.97%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,20 +1,33 @@
 class Solution {
 public:
-    void mpFounder(vector<int>& prices, int& i, int& minPrice, int& maxProfit){
-        //Base Case
-        if(i>=prices.size()) return;
+    void mpFounder(vector<int>& prices, int& i, int& minPrice, int& maxProfit) {
+        // Base Case
+        if (i >= prices.size())
+            return;
 
         // One Case
-        if(prices[i] < minPrice) minPrice = prices[i];
-        if((prices[i] - minPrice) > maxProfit) maxProfit = (prices[i] - minPrice);
+        if (prices[i] < minPrice)
+            minPrice = prices[i];
+        if ((prices[i] - minPrice) > maxProfit)
+            maxProfit = (prices[i] - minPrice);
 
-        //RE
+        // RE
         mpFounder(prices, ++i, minPrice, maxProfit);
     }
 
     int maxProfit(vector<int>& prices) {
-        int minPrice = INT_MAX, maxProfit = 0, i=0;
-        mpFounder(prices, i, minPrice, maxProfit);
+        int minPrice = INT_MAX, maxProfit = 0, i = 0;
+        // RE
+        //  mpFounder(prices, i, minPrice, maxProfit);
+
+        // Iterative
+        for (; i < prices.size(); i++) {
+            if (prices[i] < minPrice)
+                minPrice = prices[i];
+            if ((prices[i] - minPrice) > maxProfit)
+                maxProfit = (prices[i] - minPrice);
+        }
+
         return maxProfit;
     }
 };
