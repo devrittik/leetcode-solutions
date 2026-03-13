0d0a8b9bf66b21fe88a3d22b3e703f6cc6a1410b
Rittik Chakraborty <rittikchakraborty24@gmail.com>
2026-03-13 16:43:52 +0530

    Time: 51 ms (5.44%), Space: 16.6 MB (6.91%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,6 +1,13 @@
 class Solution {
 public:
-    int nRT(int n, int& k, int target, map<pair<int,int>,int>& dp){
+
+    struct pair_hash{
+        size_t operator()(const pair<int,int>& p) const {
+            return hash<int>()(p.first) ^ (hash<int>()(p.second) << 1);
+        }
+    };
+
+    int nRT(int n, int& k, int target, unordered_map<pair<int,int>,int, pair_hash>& dp){
         
         //Base Case
         if(target < 0) return 0;
@@ -20,8 +27,9 @@ public:
         return dp[{n,target}] = ans;
 
     }
+
     int numRollsToTarget(int n, int k, int target) {
-        map<pair<int,int>,int> dp;
+        unordered_map<pair<int,int>,int, pair_hash> dp;
         return nRT(n,k,target,dp);
     }
 };
